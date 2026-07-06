7b0f5513b8c07db70ef3d3d41ab95747b825236a
cmolina-tomorrowsoffice <cmolina@tomorrowsoffice.com>
2026-07-07 07:11:36 +0800

    Committing 26 items from workspace cbfb9c90-6251-4d6a-83ab-a10a2e867b1d

--- DP_700/fab_warehouse.Warehouse/rls/Functions/fn_securitypredicate.sql
+++
@@ -1,7 +0,0 @@
-CREATE   FUNCTION rls.fn_securitypredicate(@SalesRep AS VARCHAR(60)) 
-    RETURNS TABLE  
-WITH SCHEMABINDING  
-AS  
-    RETURN SELECT 1 AS fn_securitypredicate_result
--- ;
-WHERE @SalesRep = '<username1>@<your_domain>.com';

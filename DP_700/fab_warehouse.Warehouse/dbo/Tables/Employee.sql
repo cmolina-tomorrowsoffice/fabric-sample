dc21184f4e70e80dc37570f12071b692f212a334
cmolina-tomorrowsoffice <cmolina@tomorrowsoffice.com>
2026-07-28 00:11:06 +0800

    Committing 7 items from workspace 802a723a-7051-4347-9b52-da12ff6e59c3

---
+++ DP_700/fab_warehouse.Warehouse/dbo/Tables/Employee.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Employee] (
+
+	[EmployeeID] int NOT NULL, 
+	[FirstName] varchar(40) NULL, 
+	[LastName] varchar(40) NULL, 
+	[Position] varchar(60) NULL, 
+	[ModifiedDate] datetime2(3) NULL
+);
---
+++ Public Holidays notebook.Notebook/notebook-content.py
@@ -0,0 +1,62 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "4b0c81b7-cc53-4316-924b-14dabea091ab",
+# META       "default_lakehouse_name": "fab_lakehouse",
+# META       "default_lakehouse_workspace_id": "802a723a-7051-4347-9b52-da12ff6e59c3",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "4b0c81b7-cc53-4316-924b-14dabea091ab"
+# META         }
+# META       ]
+# META     },
+# META     "warehouse": {
+# META       "default_warehouse": "4536513c-3bb4-aa78-4241-d6ca589fcaee",
+# META       "known_warehouses": [
+# META         {
+# META           "id": "4536513c-3bb4-aa78-4241-d6ca589fcaee",
+# META           "type": "Datawarehouse"
+# META         }
+# META       ]
+# META     },
+# META     "event_stream": {
+# META       "known_event_streams": [
+# META         {
+# META           "artifact_id": "02179f56-ca5f-4085-a75e-e3221d713bfa",
+# META           "stream_id": "02179f56-ca5f-4085-a75e-e3221d713bfa"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# PARAMETERS CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+name = "Something"
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+print(name)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
